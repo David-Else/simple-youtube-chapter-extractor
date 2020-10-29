3a0c1a7f8da0f5795bfa74f5ac0af7ee2243c781
David <12832280+David-Else@users.noreply.github.com>
2020-10-29 12:50:00 +0000

    Update Deno test deps

--- youtube-chapter-extractor_test.ts
+++ youtube-chapter-extractor_test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.69.0/testing/asserts.ts";
+} from "https://deno.land/std@0.75.0/testing/asserts.ts";
 import { ChapterFile } from "./youtube-chapter-extractor.ts";
 
 Deno.test({
